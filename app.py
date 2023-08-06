765bd6d263d0cd25cadc62315bb96e7c67b72904
Venkat <venkicloudit@gmail.com>
2023-08-06 19:20:36 +0000

    modified

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to Python Flask Application - Ashok IT"
+    return "Dockerizing the python application"
 
 
 if __name__ == "__main__":
